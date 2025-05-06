e0ad42a1f20aa0564733687f1e146fa4842795f8
liuwenhong <1042121470@qq.com>
2025-05-06 14:09:02 +0800

    feat: commonent

---
+++ deploy.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env sh
+
+# 确保脚本抛出遇到的错误
+set -e
+
+push_addr= `git remote get-url --push origin`
+commit_info=`git describe --all --long --always`
+dist_path=docs/.vuepress/dist
+push_branch=gh-pages
+
+
+# 生产静态文件
+npm run docs:build
+
+# 进入生成的文件夹
+cd $dist_path
+
+git init
+git add -A
+git commit -m "deploy, $commit_info"
+
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
+git push -f $push_addr HEAD:$push_branch
+
+
+cd -
+
+rm -rf $dist_path
